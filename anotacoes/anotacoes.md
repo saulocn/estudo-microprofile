62cc25fb71b688d3bdd311023c0597813100b208
Saulo de Carvalho Neto <sacarvalho@uolinc.com>
2020-08-08 10:48:19 -0300

    Criando um projeto com Quarkus

---
+++ anotacoes/anotacoes.md
@@ -0,0 +1,3 @@
+Criação de um projeto Quarkus com o Maven
+
+mvn io.quarkus:quarkus-maven-plugin:1.6.0.Final:create
---
+++ microprofile/src/test/java/com/saulocn/microprofile/NativeHelloResourceIT.java
@@ -0,0 +1,9 @@
+package com.saulocn.microprofile;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeHelloResourceIT extends HelloResourceTest {
+
+    // Execute the same tests but in native mode.
+}
