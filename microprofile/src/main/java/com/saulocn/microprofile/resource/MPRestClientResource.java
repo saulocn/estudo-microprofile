d966fd0148eb3163acc8e92aac09289fd2dcd422
Saulo de Carvalho <saulocn@gmail.com>
2021-01-21 18:14:37 -0300

    Implementação do rest client sem cdi

--- microprofile/src/main/java/com/saulocn/microprofile/resource/MPRestClientResource.java
+++ microprofile/src/main/java/com/saulocn/microprofile/resource/MPRestClientResource.java
@@ -5,13 +5,14 @@ import com.saulocn.microprofile.service.MunicipioService;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.spi.CDI;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
 @Path("mp-restclient")
 @ApplicationScoped
-public class MPRestCLientResource {
+public class MPRestClientResource {
 
     @Inject
     @RestClient
@@ -23,6 +24,17 @@ public class MPRestCLientResource {
         MunicipioDTO municipio = new MunicipioDTO();
         municipio.setNome("Maceió");
         municipio.setPopulacao(1234);
+        return municipioService.adicionar(idUf, municipio).toString();
+    }
+
+    @GET
+    @Path("sem-cdi")
+    public String adicionarComGetSemCDI() {
+        Integer idUf = 12;
+        MunicipioDTO municipio = new MunicipioDTO();
+        municipio.setNome("Maceió");
+        municipio.setPopulacao(1234);
+        MunicipioService service = CDI.current().select(MunicipioService.class, RestClient.LITERAL).get();
         return municipioService.adicionar(idUf, municipio).toString();
     }
 
