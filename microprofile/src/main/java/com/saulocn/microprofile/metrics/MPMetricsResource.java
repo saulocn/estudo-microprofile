dcad76539b1d2192c39aff1a1c4042f8fcceb6a8
Saulo de Carvalho <saulocn@gmail.com>
2021-03-13 15:38:57 -0300

    Métrica de contador de chamadas ao método

---
+++ microprofile/src/main/java/com/saulocn/microprofile/metrics/MPMetricsResource.java
@@ -0,0 +1,30 @@
+package com.saulocn.microprofile.metrics;
+
+import org.eclipse.microprofile.metrics.annotation.Counted;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/mp-metrics")
+@ApplicationScoped
+public class MPMetricsResource {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Counted(
+            name = "meuContador",
+            absolute = true
+    )
+    //@ConcurrentGauge
+    public String methodName() {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "Hello";
+    }
+}
