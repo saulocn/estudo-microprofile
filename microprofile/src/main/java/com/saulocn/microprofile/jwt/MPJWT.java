40554747c7d12c85cb4b67c2d72db595f6b03998
Saulo de Carvalho <saulocn@gmail.com>
2021-05-04 21:46:24 -0300

    Injetando os parâmetros do JWT

--- microprofile/src/main/java/com/saulocn/microprofile/jwt/MPJWT.java
+++ microprofile/src/main/java/com/saulocn/microprofile/jwt/MPJWT.java
@@ -1,8 +1,14 @@
 package com.saulocn.microprofile.jwt;
 
+import org.eclipse.microprofile.jwt.Claim;
+import org.eclipse.microprofile.jwt.Claims;
+import org.eclipse.microprofile.jwt.JsonWebToken;
+
 import javax.annotation.security.DenyAll;
 import javax.annotation.security.PermitAll;
 import javax.annotation.security.RolesAllowed;
+import javax.enterprise.inject.Instance;
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -11,10 +17,26 @@ import javax.ws.rs.core.MediaType;
 @Path("mp-jwt")
 public class MPJWT {
 
+    @Inject
+    JsonWebToken token;
+
+    @Inject
+    @Claim(value = "sub", standard = Claims.preferred_username)
+    String nome;
+
+    @Inject
+    @Claim(value = "sub", standard = Claims.preferred_username)
+    Instance<String> nomeInst;
+
+    //@Inject
+    //@Claim(value = "sub", standard = Claims.preferred_username)
+    //Optional<String> nomeOpt;
+
     @GET
     @Path("permit-all")
     @PermitAll
     @Produces(MediaType.TEXT_PLAIN)
+
     public String permitAll() {
         return "hello";
     }
@@ -32,7 +54,7 @@ public class MPJWT {
     @RolesAllowed("proprietario")
     @Produces(MediaType.TEXT_PLAIN)
     public String rolesProprietario() {
-        return "hello";
+        return "hello, " + token.getName() + " - " + nome + " - " + nomeInst.get();// + " - " + nomeOpt.get();
     }
 
     @GET
